6c640c8289d15e52241586d4372d12d2733439e6
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-02-17 22:57:00 +0100

    Create porscheCalc.java

---
+++ old_kogni_stuff/porscheCalc.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+        int oszczednosciNaMiesiac = 1000; 
+        int cenaPorsche = 645170; 
+        int liczbaMiesiecyOszczedzania = (cenaPorsche / oszczednosciNaMiesiac);
+
+        System.out.println("Trzeba oszczędzać " + liczbaMiesiecyOszczedzania + " miesięcy");
+  }
+}
