b5df1df8f854cdcf7716da69aeedca0160b801ad
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-09-07 11:34:12 +0200

    Create getFirst.java

---
+++ getFirst.java
@@ -0,0 +1,6 @@
+public static String getFirst(String[] array) {
+    if (array.length > 0) {
+        return array[0];
+    }
+    return null; // or throw an exception, or return a default value
+}
